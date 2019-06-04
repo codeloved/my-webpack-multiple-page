bf2affeaad8e93d6c1ed7282268d068852f4237c
郑城城 <zhengcc@xbzoom.com>
2019-06-04 17:46:16 +0800

    add

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 1.利用webpack打包多页应用, 基于webpack4
 功能: 
 1.
-2.
+2.css中的url处理,file-loader中输出是否可以保持文件的路径
--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,10 @@ module.exports = {
             options: {
               limit: 8192, // 小于8192b的图片,会直接打包到js文件,超过使用file-loader处理,并且所有的参数都会传递给file-loader,所以file-loader的参数也在此处配置
               name: '[name]-[hash].[ext]',  // file-loader配置
-              outputPath: 'assets/images',
+              // outputPath: 'assets/images',
+              outputPath: (url, resourcePath, context) => {
+
+              }
             }
           }
         ]
